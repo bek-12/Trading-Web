fdb845b0ae550c622f25335a1fa80ed652c87b06
bek-12 <106967571+bek-12@users.noreply.github.com>
2026-06-05 15:38:12 +0300

    Fix PATCH CORS: add PATCH to allowed methods

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -24,6 +24,6 @@ export function requireAuth(req, res) {
 
 export function cors(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 }
